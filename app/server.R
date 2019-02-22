fd193ce49bec38e279d883c5104f6de64b6cecc0
nelsonlin2708968 <nelsonlin2708968@gmail.com>
2019-02-21 22:55:52 -0500

    FIXED UP SOME STUFF

--- app/server.R
+++ app/server.R
@@ -1,6 +1,6 @@
 packages.used=c("rgeos", "sp", "rgdal", 
                 "leaflet", "htmlwidgets", "shiny",
-                "ggplot2", "dplyr", "data.table","DT")
+                "ggplot2", "dplyr", "data.table","DT", "tidyverse")
 
 # check packages that need to be installed.
 packages.needed=setdiff(packages.used, 
@@ -21,19 +21,21 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(data.table)
-
+library(readr)
 
 
 #setwd("~/Desktop/Spr2017-proj2-grp2")
 setwd("../")
 
-
+# set group based on radio selection
 group1 = "<span style='color: #7f0000; font-size: 11pt'><strong>count</strong></span>"
 group2 = "<span style='color: #7f0000; font-size: 11pt'><strong>FPD</strong></span>"
 group3 = "<span style='color: #7f0000; font-size: 11pt'><strong>Percentage Cash Paying:</strong></span>"
+# set color scheme
 color = list(color1 = c('#F2D7D5','#D98880', '#CD6155', '#C0392B', '#922B21','#641E16'),
              color2 = c('#e6f5ff','#abdcff', '#70c4ff', '#0087e6', '#005998','#00365d','#1B4F72'),
              color3 = c("#F7FCF5","#74C476", "#005A32"))
+# set bin sizes and make legend in the bottom right
 bin = list(bin1 = c(0,100,1000,10000,100000,1000000,10000000), bin2 = c(0,1,2,3,4,5,6,7))
 label = list(label1 = c("<100","100-1000","1000~10,000","10,000~100,000","100,000~1,000,000","1,000,000~10,000,000"),
              label2 = c("0-1","2-3","3-4","4-5","5-6","6","7+"),
@@ -42,23 +44,36 @@ title = list(t1 = "Pick Up Numbers", t2 = "Fair Per Distance",t3  = "PercentageP
 
 
 
-
+# load shape data 
 load('output/myShape1.RData')
 subdat<-spTransform(myShape1, CRS("+init=epsg:4326"))
-dynamicdata = fread("data/pickupDropoff date_hour.csv", header = TRUE, stringsAsFactors=F)
-#load('output/count_seperated_2015.RData')
 
+# load dynamic data
+dynamicdata = fread("data/pickupDropoff date_hour.csv", header = TRUE, stringsAsFactors=F)
+
+# load interactive data
 load('output/count_seperated.RData')
 load('output/FPD_seperated.RData')
+
+# load airport traffic data
+read_csv('output/AirportSummarized/JFK_count_seperated')
+read_csv('output/AirportSummarized/JFK_FPD_seperated')
+read_csv('output/AirportSummarized/NWK_count_seperated')
+read_csv('output/AirportSummarized/NWK_FPD_seperated')
+read_csv('output/AirportSummarized/LGA_count_seperated')
+read_csv('output/AirportSummarized/LGA_FPD_seperated')
+
+# names of each borough. subdat is transformed shape1 file
 rownames(count_result) = subdat@data$NTACode
 
-subway1 = read.csv("data/sub.csv", header = TRUE, stringsAsFactors = F)
-subway2 = read.csv("data/sub2.csv", header = TRUE, stringsAsFactors = F)
+# removal of subway data
+# subway1 = read.csv("data/sub.csv", header = TRUE, stringsAsFactors = F)
+# subway2 = read.csv("data/sub2.csv", header = TRUE, stringsAsFactors = F)
+
+# summarized data 
 payper = read.csv("data/Data_frame_of_summary.csv")
 
-
-
-
+# THIS IS THE CLUSTERING DATA TRANSFORMATION. Kinda Useless
 count_result1 <- as.data.frame(count_result[,,1])
 
 sum <- apply(count_result1, 1, sum)
@@ -72,11 +87,10 @@ aggregate(count_result1,by=list(fit$cluster),FUN=mean)
 count_result1 <- data.frame(count_result1, fit$cluster)
 
 
-
 #head(subdat@data)
 shinyServer(function(input, output,session) { 
   
-  
+  #OUTPUT CALLED MAP
   output$map <- renderLeaflet({
     
     #  if (!input$showhr){
@@ -85,7 +99,8 @@ shinyServer(function(input, output,session) {
     #  else{
     #    subdat@data$count = count_result[, input$hr_adjust+1, ifelse(input$days == "Business Day", 1, 2)]
     #  }
-   
+    
+    # THESE ARE BACK END FOR DATA TO LOAD ON INTERACTIVE MAP
     if (input$days == "All day"){
       count_intermediate = count_result %>% apply(c(1,2), sum)
       FPD_intermediate = FPD_result %>% apply(c(1,2), mean, na.rm = T)
@@ -93,6 +108,7 @@ shinyServer(function(input, output,session) {
       count_intermediate = count_result[ , , (input$days == "Not Business Day") + 1]
       FPD_intermediate = FPD_result[ , , (input$days == "Not Business Day") + 1]
     }
+    # THIS IS BACK END FOR HOURLY INFORMATION MAP
     if (!input$showhr){
       subdat@data$count = count_intermediate %>% apply(1, sum)
       subdat@data$FPD = FPD_intermediate %>% apply(1, mean, na.rm = T)
@@ -106,6 +122,8 @@ shinyServer(function(input, output,session) {
     blocks_coord = data.frame(center_lng = rep(NA, 195), center_lat = rep(NA, 195)) # Combine borough coord for future marking purpose
     for (i in 1:195){ blocks_coord[i,] = subdat@polygons[[i]]@labpt }    # One more update: add long/lat permanently into myShape@data as
 
+    
+    
     subdat_top5_intermediate = cbind(subdat@data, blocks_coord)
 
      if (input$boroSelect == "All"){ # filter borough
@@ -150,40 +168,28 @@ shinyServer(function(input, output,session) {
                     '<br><strong>Percentage Paying Cash: </strong><br>', payper$PercentagePaying)
     
     
-    greenLeafIcon <- makeIcon(
-      iconUrl = "https://cdn1.iconfinder.com/data/icons/weather-19/32/fire-512.png",
-      iconWidth = 38, iconHeight = 38,
-      iconAnchorX = 0, iconAnchorY = 0
-      # shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-      # shadowWidth = 50, shadowHeight = 64,
-      # shadowAnchorX = 4, shadowAnchorY = 62
-    )
-    redLeafIcon <- makeIcon(
-      iconUrl = "https://maxcdn.icons8.com/Share/icon/Finance//usd1600.png",
-      iconWidth = 38, iconHeight = 38,
-      iconAnchorX = 0, iconAnchorY = 0
-      # shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-      # shadowWidth = 50, shadowHeight = 64,
-      # shadowAnchorX = 4, shadowAnchorY = 62
-    )
-    
-
-    # pic1<-leaflet(subdat) %>%
-    #   setView(lat=40.7128, lng=-74.0059, zoom=10) %>%
-    #   addProviderTiles('CartoDB.Positron') %>%
-    #   addPolygons(fillColor = ~pal(count), color = 'grey', weight = 1, 
-    #               popup = popup1, fillOpacity = .6, group = group1) %>%
-    #   addPolygons(fillColor = ~pal_FPD(FPD), color = 'grey', weight = 1, 
-    #               popup = popup2, fillOpacity = .6, group = group2) %>%
-    #   addLayersControl(baseGroups = c(group1,group2), 
-    #                    options = layersControlOptions(collapsed = F)) 
-
+    # greenLeafIcon <- makeIcon(
+    #   iconUrl = "https://cdn1.iconfinder.com/data/icons/weather-19/32/fire-512.png",
+    #   iconWidth = 38, iconHeight = 38,
+    #   iconAnchorX = 0, iconAnchorY = 0
+    #   # shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    #   # shadowWidth = 50, shadowHeight = 64,
+    #   # shadowAnchorX = 4, shadowAnchorY = 62
+    # )
+    # redLeafIcon <- makeIcon(
+    #   iconUrl = "https://maxcdn.icons8.com/Share/icon/Finance//usd1600.png",
+    #   iconWidth = 38, iconHeight = 38,
+    #   iconAnchorX = 0, iconAnchorY = 0
+    #   # shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    #   # shadowWidth = 50, shadowHeight = 64,
+    #   # shadowAnchorX = 4, shadowAnchorY = 62
+    # )
     
     pic1<-leaflet(subdat) %>%
       setView(lat=40.7128, lng=-74.0059, zoom=10) %>%
       addProviderTiles('CartoDB.Positron') 
 
-    
+    # IF VALUE SELECTED WAS COUNT
     if (input$CF == "count"){
       pic1<-pic1 %>%
         addPolygons(fillColor = ~pal(count), color = 'grey', weight = 1, 
@@ -194,6 +200,8 @@ shinyServer(function(input, output,session) {
                   opacity = 0.6,
                   title = title[[1]])
     }
+    
+    # IF VALUE SELECTED WAS FPD
     else if (input$CF == "FPD"){
       pic1<-pic1 %>%
         addPolygons(fillColor = ~pal_FPD(FPD), color = 'grey', weight = 1, 
@@ -205,11 +213,12 @@ shinyServer(function(input, output,session) {
                   title = title[[2]])
     }
     
-    else if(input$CF == "cluster1"){
-      pic1<-pic1 %>%
-      addPolygons(fillColor = ~pal2(count_result1$fit.cluster), color = 'grey', weight = 1, popup = popup3, fillOpacity = .6)
-
-    }
+    # REMOVING CLUSTER BECAUSE STUPID
+    # else if(input$CF == "cluster1"){
+    #   pic1<-pic1 %>%
+    #   addPolygons(fillColor = ~pal2(count_result1$fit.cluster), color = 'grey', weight = 1, popup = popup3, fillOpacity = .6)
+    # 
+    # }
     
     else if (input$CF == "cash"){
       pic1<-pic1 %>%
@@ -224,78 +233,62 @@ shinyServer(function(input, output,session) {
 
   
 ###TOP5    
-    if (input$top15count == TRUE){
-    pic1<-pic1 %>%
-    addMarkers(~top5count$center_lng, ~top5count$center_lat, icon = greenLeafIcon)
-    }
-
-    else{
-      pic1
-    }
-    
-    if (input$top15FPD == TRUE){
-      pic1<-pic1 %>%
-        addMarkers(~top5FPD$center_lng, ~top5FPD$center_lat, icon = redLeafIcon)
-    }
-
-    else{
-      pic1
-    }
+    # if (input$top15count == TRUE){
+    # pic1<-pic1 %>%
+    # addMarkers(~top5count$center_lng, ~top5count$center_lat, icon = greenLeafIcon)
+    # }
+    # 
+    # else{
+    #   pic1
+    # }
+    # 
+    # if (input$top15FPD == TRUE){
+    #   pic1<-pic1 %>%
+    #     addMarkers(~top5FPD$center_lng, ~top5FPD$center_lat, icon = redLeafIcon)
+    # }
+    # 
+    # else{
+    #   pic1
+    # }
 
 ### subway analysis    
-    if (input$subway == 1){
-      pic1
-    }
-    else if (input$subway == 2) {
-      pic1<-pic1 %>% addMarkers(data = subway1, ~Latitude, ~Longitude, clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F))  
-    }
-    else if (input$subway == 3){
-    pic1<-pic1 %>% addMarkers(data = subway2, ~L1, ~L2, clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F))  
-      
-    }
-    
+    # if (input$subway == 1){
+    #   pic1
+    # }
+    # else if (input$subway == 2) {
+    #   pic1<-pic1 %>% addMarkers(data = subway1, ~Latitude, ~Longitude, clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F))  
+    # }
+    # else if (input$subway == 3){
+    # pic1<-pic1 %>% addMarkers(data = subway2, ~L1, ~L2, clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F))  
+    #   
+    # }
 
   })
   
   
-  
-  # observeEvent(input$map_groups,{
-  #   map <- leafletProxy("map")%>% clearControls()
-  #   if (input$map_groups == group1)
-  #   {map<- map %>%
-  #     addLegend(position = "bottomright",
-  #               colors = color[[1]],
-  #               labels = label[[1]],
-  #               opacity = 0.6,
-  #               title = title[[1]])
-  #   }
-  #   
-  #   else if (input$map_groups == group2)
-  #   {map <- map %>%
-  #     addLegend(position = 'bottomright',
-  #               colors = color[[2]],
-  #               labels = label[[2]], ## legend labels (only min and max)
-  #               opacity = 0.6,      ##transparency again
-  #               title = title[[2]])
-  #   }
-  # })
-  
-  
   observe({
+    #whenever map item is clicked, becomes event
     event <- input$map_shape_click
     if (is.null(event))
       return()
     
+    #create a data fram for longitude and latitude of click location
     dattest = data.frame(Longitude = event$lng, Latitude = event$lat)
     coordinates(dattest) <- ~ Longitude + Latitude
     proj4string(dattest) <- CRS("+proj=longlat")
     dattest <- spTransform(dattest, proj4string(myShape1))
+    
+    #rtest is to see which borough click falls in
     rtest = over(dattest, myShape1)
     
+    #create plot based borough selected
     output$districttimeplot <- renderPlot({
+      #returns null if no borough selected
       if (nrow(rtest) == 0) {
         return(NULL)
       }
+      
+      # returns histogram plot
       if (input$days == "All Day"){
         count_resultNTA = count_result[which(rownames(count_result) == rtest$NTACode),,]
         count_resultNTA = apply(count_resultNTA, 1, sum)
@@ -321,7 +314,7 @@ shinyServer(function(input, output,session) {
       
     })
   })
-    
+    # THIS IS LEAFLET FOR DYNAMIC MAP
     output$map2 <- renderLeaflet({
       leaflet() %>%
         setView(lat=40.7128, lng=-74.0059, zoom=11) %>%
@@ -407,14 +400,180 @@ shinyServer(function(input, output,session) {
       }
       
     })
+    
+    
+    #OUTPUT CALLED MAP
+    output$airmap <- renderLeaflet({
+      
+      
+      # THESE ARE BACK END FOR DATA TO LOAD ON INTERACTIVE MAP
+      if (input$days == "All day"){
+        count_intermediate = count_result %>% apply(c(1,2), sum)
+        FPD_intermediate = FPD_result %>% apply(c(1,2), mean, na.rm = T)
+      }else{
+        count_intermediate = count_result[ , , (input$days == "Not Business Day") + 1]
+        FPD_intermediate = FPD_result[ , , (input$days == "Not Business Day") + 1]
+      }
+      # THIS IS BACK END FOR HOURLY INFORMATION MAP
+      if (!input$showhr){
+        subdat@data$count = count_intermediate %>% apply(1, sum)
+        subdat@data$FPD = FPD_intermediate %>% apply(1, mean, na.rm = T)
+      }else{
+        subdat@data$count = count_intermediate[, input$hr_adjust+1]
+        subdat@data$FPD = FPD_intermediate[, input$hr_adjust+1]
+      }
+      
+      ######
+      
+      blocks_coord = data.frame(center_lng = rep(NA, 195), center_lat = rep(NA, 195)) # Combine borough coord for future marking purpose
+      for (i in 1:195){ blocks_coord[i,] = subdat@polygons[[i]]@labpt }    # One more update: add long/lat permanently into myShape@data as
+      
+      
+      
+      subdat_top5_intermediate = cbind(subdat@data, blocks_coord)
+      
+      if (input$boroSelect == "All"){ # filter borough
+        subdat_top5 = subdat_top5_intermediate
+      } else {
+        subdat_top5 = subdat_top5_intermediate[subdat@data$BoroName == input$boroSelect, ]
+      }
+      if (!input$showbr){
+        subdat_top5 = subdat_top5_intermediate
+      }
+      
+      subdat_top5 = subdat_top5 %>% 
+        subset(select = c("NTACode", "NTAName", "count", "FPD", "center_lng", "center_lat"))
+      
+      top5count = subdat_top5[order(subdat_top5$count, decreasing = T),
+                              c("NTAName", "count", "center_lng", "center_lat")] %>% head(5) # fetch top-5-rows with the most counts/FPD
+      top5FPD = subdat_top5[order(subdat_top5$FPD, decreasing = T),
+                            c("NTAName", "FPD", "center_lng", "center_lat")] %>% head(5)
+      
+      
+      ######
+      
+      
+      #subdat@data$count = count_result[, input$hr_adjust+1,  1]
+      
+      subdat_data=subdat@data[,c("NTACode", "NTAName", "count", "FPD")]
+      subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
+      
+      # print leaflet
+      pal = colorBin(color[[1]], bins = bin[[1]])
+      pal_FPD = colorBin(color[[2]], bins = bin[[2]])
+      pal2 = colorBin(c("#882E72", "#B178A6", "#D6C1DE", "#1965B0", "#5289C7", "#7BAFDE", "#4EB265", "#90C987", "#CAE0AB", "#F7EE55", "#F6C141", "#F1932D", "#E8601C", "#DC050C"), 1:10)
+      pal3 = colorBin(c("#005A32", "#74C476", "#F7FCF5"), 0:0.125:1)
+      
+      
+      popup1 = paste0('<strong>Neighborhood: </strong><br>', subdat_data$NTAName, 
+                      '<br><strong>Count of pick-ups: </strong><br>', subdat_data$count)
+      popup2 = paste0('<strong>Neighborhood: </strong><br>', subdat_data$NTAName, 
+                      '<br><strong>Fair Per Distance: </strong><br>', subdat_data$FPD)
+      popup3 = paste0('<strong>Neighborhood: </strong><br>', subdat_data$NTAName)
+      popup4 = paste0('<strong>Neighborhood: </strong><br>', subdat_data$NTAName, 
+                      '<br><strong>Percentage Paying Cash: </strong><br>', payper$PercentagePaying)
+      
+      
+
+      pic1<-leaflet(subdat) %>%
+        setView(lat=40.7128, lng=-74.0059, zoom=10) %>%
+        addProviderTiles('CartoDB.Positron') 
+      
+      # IF VALUE SELECTED WAS COUNT
+      if (input$CF == "count"){
+        pic1<-pic1 %>%
+          addPolygons(fillColor = ~pal(count), color = 'grey', weight = 1, 
+                      popup = popup1, fillOpacity = .6, group = group1) %>%
+          addLegend(position = "bottomright",
+                    colors = color[[1]],
+                    labels = label[[1]],
+                    opacity = 0.6,
+                    title = title[[1]])
+      }
+      
+      # IF VALUE SELECTED WAS FPD
+      else if (input$CF == "FPD"){
+        pic1<-pic1 %>%
+          addPolygons(fillColor = ~pal_FPD(FPD), color = 'grey', weight = 1, 
+                      popup = popup2, fillOpacity = .6, group = group2) %>%
+          addLegend(position = 'bottomright',
+                    colors = color[[2]],
+                    labels = label[[2]], ## legend labels (only min and max)
+                    opacity = 0.6,      ##transparency again
+                    title = title[[2]])
+      }
+    
+      
+      else if (input$CF == "cash"){
+        pic1<-pic1 %>%
+          addPolygons(fillColor =  ~pal3(payper$PercentagePayingCash), color = 'grey', weight = 1, 
+                      popup = popup4, fillOpacity = .6, group = group3) %>%
+          addLegend(position = 'bottomright',
+                    colors = color[[3]],
+                    labels = label[[3]], ## legend labels (only min and max)
+                    opacity = 0.6,      ##transparency again
+                    title = title[[3]])
+      }
+      
+      
+    })
+    
+    
+    observe({
+      #whenever map item is clicked, becomes event
+      event <- input$map_shape_click
+      if (is.null(event))
+        return()
+      
+      #create a data fram for longitude and latitude of click location
+      dattest = data.frame(Longitude = event$lng, Latitude = event$lat)
+      coordinates(dattest) <- ~ Longitude + Latitude
+      proj4string(dattest) <- CRS("+proj=longlat")
+      dattest <- spTransform(dattest, proj4string(myShape1))
+      
+      #rtest is to see which borough click falls in
+      rtest = over(dattest, myShape1)
+      
+      #create plot based borough selected
+      output$districttimeplot <- renderPlot({
+        #returns null if no borough selected
+        if (nrow(rtest) == 0) {
+          return(NULL)
+        }
+        
+        # returns histogram plot
+        if (input$days == "All Day"){
+          count_resultNTA = count_result[which(rownames(count_result) == rtest$NTACode),,]
+          count_resultNTA = apply(count_resultNTA, 1, sum)
+          index <- c(0:23)
+          dfcount_resultNTA <- data.frame(index, count_resultNTA)
+          ggplot(data=dfcount_resultNTA, aes(x=index, y=count_resultNTA)) + geom_bar(stat="identity") + 
+            labs(x = "hour") + labs(y = "count per hour")+ggtitle("pick up count flow trend")+geom_smooth(formula = y~x)
+        }
+        else if (input$days == "Business Day"){
+          count_resultNTA = count_result[which(rownames(count_result) == rtest$NTACode),,1]
+          index <- c(0:23)
+          dfcount_resultNTA <- data.frame(index, count_resultNTA)
+          ggplot(data=dfcount_resultNTA, aes(x=index, y=count_resultNTA)) + geom_bar(stat="identity") + 
+            labs(x = "hour") + labs(y = "count per hour")+ggtitle("pick up count flow trend")+geom_smooth(formula = y~x)
+        }
+        else if (input$days == "Not Business Day") {
+          count_resultNTA = count_result[which(rownames(count_result) == rtest$NTACode),,2]
+          index <- c(0:23)
+          dfcount_resultNTA <- data.frame(index, count_resultNTA)
+          ggplot(data=dfcount_resultNTA, aes(x=index, y=count_resultNTA)) + geom_bar(stat="identity") + 
+            labs(x = "hour") + labs(y = "count per hour")+ggtitle("pick up count flow trend")+geom_smooth(formula = y~x)
+        }
+        
+      })
+    })
   
   
   
-  
-  dataa<-fread("data/rawdata.csv",header = T)
-  output$rawtable <- DT::renderDataTable({
-    DT::datatable(dataa)
-  })
+  # dataa<-fread("data/rawdata.csv",header = T)
+  # output$rawtable <- DT::renderDataTable({
+  #   DT::datatable(dataa)
+  # })
   
 })
 
--- app/ui.r
+++ app/ui.r
@@ -28,7 +28,7 @@ navbarPage("NYC TAXI", id="nav",
                                       width = 160, height = 180,
                                       
                                       radioButtons("CF", label = "Layers",
-                                                   choices = list("Count Number" = "count", "Fare Per Distance" = "FPD","Cluster" = "cluster1" ,"Cash Paying Percentage" = "cash"), 
+                                                   choices = list("Count Number" = "count", "Fare Per Distance" = "FPD", "Cash Paying Percentage" = "cash"), 
                                                    selected = "count")
                                       
                                       
@@ -63,9 +63,9 @@ navbarPage("NYC TAXI", id="nav",
                                       
                                       
                                       
-                                      checkboxInput("top15count", "Top 5 Count", FALSE),
-                                      checkboxInput("top15FPD", "Top 5 FPD", FALSE),
-                                      
+                                      # checkboxInput("top15count", "Top 5 Count", FALSE),
+                                      # checkboxInput("top15FPD", "Top 5 FPD", FALSE),
+                                      # 
                                       
                                       checkboxInput(inputId = "showbr",
                                                     label = strong("Show Borough for Top 5 counts/FPD"),
@@ -80,9 +80,9 @@ navbarPage("NYC TAXI", id="nav",
                                       
                                     
                                       
-                                      radioButtons("subway", label = h4("Subway Station : "),
-                                                   choices = list("Do not appear" = 1, "Show all stations" = 2, "Show unique station" = 3), 
-                                                   selected = 1),
+                                      # radioButtons("subway", label = h4("Subway Station : "),
+                                      #              choices = list("Do not appear" = 1, "Show all stations" = 2, "Show unique station" = 3), 
+                                      #              selected = 1),
                                       
                                       plotOutput("districttimeplot", height = 280),
                                       helpText(   a("Analysis",
@@ -134,13 +134,86 @@ navbarPage("NYC TAXI", id="nav",
                     )
            ),
            
-           tabPanel("Raw Data",
-                    hr(),
-                    DT::dataTableOutput("rawtable")
-           ),
+           # tabPanel("Raw Data",
+           #          hr(),
+           #          DT::dataTableOutput("rawtable")
+           # ),
            
-           tabPanel("Aiport",
-                    verbatimTextOutput("summary")
+           
+           
+           tabPanel("Airport Map",
+                    div(class="outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        leafletOutput("airmap", width="100%", height="100%"),
+                        
+                        # Shiny versions prior to 0.11 should use class="modal" instead.
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = F, top = 60, left = "auto", right = 0, bottom = "auto",
+                                      width = 160, height = 180,
+                                      
+                                      radioButtons("CF", label = "Layers",
+                                                   choices = list("Count Number" = "count", "Fare Per Distance" = "FPD", "Cash Paying Percentage" = "cash"), 
+                                                   selected = "count")
+                                      
+                                      
+                        ),
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = 0, right = "auto", bottom = "auto",
+                                      width = 330, height = "auto",
+                                      
+                                      h3("Panel"),
+                                      
+                                      
+                                      selectInput("days", "Days", c("All Day", "Business Day", "Not Business Day"),selected = "All Day"),
+                                      
+                                      
+                                      checkboxInput(inputId = "showhr",
+                                                    label = strong("Show hours"),
+                                                    value = FALSE),
+                                      
+                                      conditionalPanel(condition = "input.showhr == false"
+                                                       
+                                      ),
+                                      
+                                      
+                                      conditionalPanel(condition = "input.showhr == true",
+                                                       sliderInput(inputId = "hr_adjust",
+                                                                   label = "Choose the time of the day:",
+                                                                   min = 0, max = 23, value = NULL, step = 1)
+                                      ),
+                                      
+                                      
+                                      
+                                      
+                                      # checkboxInput("top15count", "Top 5 Count", FALSE),
+                                      # checkboxInput("top15FPD", "Top 5 FPD", FALSE),
+                                      # 
+                                      
+                                      checkboxInput(inputId = "showbr",
+                                                    label = strong("Show Borough for Top 5 counts/FPD"),
+                                                    value = FALSE),
+                                      
+                                      conditionalPanel(condition = "input.showbr == true",
+                                                       selectInput("boroSelect", "Borough for Top 5 counts/FPD", 
+                                                                   c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island", "All"), 
+                                                                   selected = "All")
+                                      )
+                                      
+                                      
+                                      # THIS IS MY OWN AIRPORT ANALYSIS
+                                      # plotOutput("districttimeplot", height = 280),
+
+                        )
+                        
+                        
+                    )
            )
            
            
